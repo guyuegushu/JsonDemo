63bc1f14996287622dd2d1aef3b07ec5b0278538
hushu <hushu@126.com>
2016-09-05 15:14:27 +0800

    9.05 Create Project

---
+++ app/src/main/java/com/example/veb/jsondemo2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.veb.jsondemo2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by VEB on 2016/9/5.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+}
